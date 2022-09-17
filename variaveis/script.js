23deb3869760e90800d8677d759bb6a42f70b110
Thiagobarroso <thiagoo.silva@live.com>
2022-09-16 22:09:29 -0300

    Variaveis

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,1 +1,31 @@
-console.log("Olá Mundo");
+var nome = "Thiago ";
+var sobreNome = "Barroso ";
+var faculdade = false;
+
+var dadosCliente = nome + sobreNome + faculdade;
+
+console.log(dadosCliente);
+
+/////////////////////
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+/////////////////////
+
+var preco = 25,
+  totalcomprado = 5,
+  totalPreco = preco * totalcomprado;
+console.log(totalPreco);
+
+/////////////////////
+
+var nome = "Thiago Barroso ",
+  cidade = "Cidade: Rio de janeiro";
+
+console.log(nome, cidade);
+
+////// undefined /////
