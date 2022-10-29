33aac360844cb8718274c98261b963d919472bee
Thiagobarroso <thiagoo.silva@live.com>
2022-10-28 21:22:19 -0300

    Excercicios de obejto javascript

--- objetos/script.js
+++ objetos/script.js
@@ -1,9 +1,26 @@
+/*Obejetos servem para organizar o código em pequenas partes reutilizáveis*/
+
 var pessoa = {
   nome: "Thiago",
   sobrenome: "Barroso",
   idade: 27,
-  profissao: "Designer",
+  profissao: "Programador",
   possuiFaculdade: true,
+  saldoConta: function (salario, desconto) {
+    return salario - desconto;
+  },
+  nomeSocial: function () {
+    return this.nome + this.sobrenome;
+  },
+
+  nomeProfissao() {
+    return this.profissao;
+  },
 };
 
-console.log(pessoa.profissao);
+console.log(pessoa.idade);
+console.log(pessoa.saldoConta(7000, 2000));
+console.log(pessoa.nomeSocial());
+console.log(pessoa.nomeProfissao());
+
+//////////////////////
