83d46970f49d2738d072fb5a7edc48b640ca5b75
thiagobarroso <thiagoo.silva@live.com>
2023-01-04 13:13:29 -0300

    estudo dia 04/01

---
+++ DOM/script.js
@@ -0,0 +1,19 @@
+//  EXCERCICIOS
+
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+const titulo = document.querySelector("h1");
+console.log(titulo.classList[0]);
+
+// Retorne a linguagem do navegador
+
+const linguagem = document.querySelector("html");
+console.log(linguagem.lang);
+
+// Retorne a largura da janela
+const largura = window.innerWidth;
+console.log(largura);
